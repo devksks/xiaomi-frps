fb06e379d68c0f52f096654d249c2679f430c748
devksks <91334244+devksks@users.noreply.github.com>
2023-08-09 18:31:02 +0800

    Update frps.sh

--- frps.sh
+++ frps.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-
 curl -o /tmp/mnt/frps.ini https://raw.githubusercontent.com/devksks/xiaomi-frps/main/frps.ini
-
-
 curl -o /tmp/mnt/frps https://raw.githubusercontent.com/devksks/xiaomi-frps/main/frps
-
-
 chmod +x /tmp/mnt/frps
-
-
 /tmp/mnt/frps -c /tmp/mnt/frps.ini
 
