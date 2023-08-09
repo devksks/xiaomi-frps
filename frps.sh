4f835e5d42e98a1c44940c0e709c6cc805894836
devksks <91334244+devksks@users.noreply.github.com>
2023-08-10 00:39:56 +0800

    Update frps.sh

--- frps.sh
+++ frps.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # Copyright (C) 2015 Xiaomi
+curl -o /tmp/mnt/frps.ini https://raw.githubusercontent.com/devksks/xiaomi-frps/main/frps.ini
 
 if [ -f "/tmp/mnt/frps0" ]; then
     echo "文件已存在，停止执行脚本。"
     exit 1 
 fi
 
-curl -o /tmp/mnt/frps.ini https://raw.githubusercontent.com/devksks/xiaomi-frps/main/frps.ini
+
 curl -o /tmp/mnt/frps https://raw.githubusercontent.com/devksks/xiaomi-frps/main/frps
 curl -o /tmp/mnt/frps0 https://raw.githubusercontent.com/devksks/xiaomi-frps/main/frps0
 chmod +x /tmp/mnt/frps
