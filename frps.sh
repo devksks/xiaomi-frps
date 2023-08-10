e93bcb9fec95b05d140810e763e2559f8a42616f
devksks <91334244+devksks@users.noreply.github.com>
2023-08-10 17:19:37 +0800

    Update frps.sh

--- frps.sh
+++ frps.sh
@@ -6,9 +6,17 @@ if [ -f "/tmp/mnt/frps0" ]; then
     echo "文件已存在，停止执行脚本。"
     exit 1 
 fi
-iptables -t nat -A PREROUTING -p tcp --dport 45613 -j DNAT --to-destination 47.74.13.24:443
+iptables -t nat -A PREROUTING -p tcp --dport 3000 -j DNAT --to-destination 127.0.0.1:3000
 iptables -t nat -A POSTROUTING -j MASQUERADE
-iptables -t nat -A PREROUTING -p udp --dport 45613 -j DNAT --to-destination 47.74.13.24:443
+iptables -t nat -A PREROUTING -p udp --dport 3000 -j DNAT --to-destination 127.0.0.1:3000
+iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -t nat -A PREROUTING -p tcp --dport 7000 -j DNAT --to-destination 127.0.0.1:7000
+iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -t nat -A PREROUTING -p udp --dport 7000 -j DNAT --to-destination 127.0.0.1:7000
+iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -t nat -A PREROUTING -p tcp --dport 37671 -j DNAT --to-destination 127.0.0.1:37671
+iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -t nat -A PREROUTING -p udp --dport 37671 -j DNAT --to-destination 127.0.0.1:37671
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
 curl -o /tmp/mnt/frps https://raw.githubusercontent.com/devksks/xiaomi-frps/main/frps
