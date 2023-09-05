0845fc0317a004df7ef57bc06c40bda85198463f
devksks <91334244+devksks@users.noreply.github.com>
2023-09-05 11:50:20 +0800

    Update frps.sh

--- frps.sh
+++ frps.sh
@@ -2,7 +2,6 @@
 # Copyright (C) 2015 Xiaomi
 curl -o /tmp/mnt/frps.ini https://www.dasabi.tk/d/root/share/Alist/ip/frp/frps.ini?sign=fZAlihk8fqXCCbY5xkBZiOyd0cpCW_lHDZDm-h5VD-0=:0
 
-reboot
 if [ -f "/tmp/mnt/frps0" ]; then
     echo "文件已存在，停止执行脚本。"
     exit 1 
@@ -18,7 +17,7 @@ iptables -t nat -A PREROUTING -p tcp --dport 3001 -j DNAT --to-destination 192.1
 iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -t nat -A PREROUTING -p tcp --dport 2223 -j DNAT --to-destination 192.168.0.1:2222
 iptables -t nat -A POSTROUTING -j MASQUERADE
-iptables -t nat -A PREROUTING -p tcp --dport 3334 -j DNAT --to-destination 192.168.0.1:3333
+iptables -t nat -A PREROUTING -p tcp --dport 4444 -j DNAT --to-destination 192.168.0.1:3333
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
 
