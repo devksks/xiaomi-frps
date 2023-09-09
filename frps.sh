618670f450ddf0d4a42413c83705caa2bd9b605a
devksks <91334244+devksks@users.noreply.github.com>
2023-09-09 18:26:28 +0800

    Update frps.sh

--- frps.sh
+++ frps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Copyright (C) 2015 Xiaomi
 curl -o /tmp/mnt/frps.ini https://www.dasabi.tk/d/root/share/Alist/ip/frp/frps.ini?sign=fZAlihk8fqXCCbY5xkBZiOyd0cpCW_lHDZDm-h5VD-0=:0
-
+reboot
 content=$(cat /tmp/mnt/frps0)
 
 if [ "$content" = "0000123456789888" ]; then
@@ -9,27 +9,16 @@ if [ "$content" = "0000123456789888" ]; then
 fi
 
 iptables -t nat -A PREROUTING -p tcp --dport 7000 -j DNAT --to-destination 192.168.0.1:7000
-iptables -t nat -A POSTROUTING -j MASQUERAD
 iptables -t nat -A PREROUTING -p tcp --dport 853 -j DNAT --to-destination 192.168.0.1:1233
-iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -t nat -A PREROUTING -p tcp --dport 37671 -j DNAT --to-destination 192.168.0.1:37670
-iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -t nat -A PREROUTING -p tcp --dport 3001 -j DNAT --to-destination 192.168.0.1:3000
-iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -t nat -A PREROUTING -p tcp --dport 2223 -j DNAT --to-destination 192.168.0.1:2222
-iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -t nat -A PREROUTING -p tcp --dport 4444 -j DNAT --to-destination 192.168.0.1:3333
-iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -t nat -A PREROUTING -p udp --dport 7000 -j DNAT --to-destination 192.168.0.1:7000
-iptables -t nat -A POSTROUTING -j MASQUERAD
 iptables -t nat -A PREROUTING -p udp --dport 853 -j DNAT --to-destination 192.168.0.1:1233
-iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -t nat -A PREROUTING -p udp --dport 37671 -j DNAT --to-destination 192.168.0.1:37670
-iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -t nat -A PREROUTING -p udp --dport 3001 -j DNAT --to-destination 192.168.0.1:3000
-iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -t nat -A PREROUTING -p udp --dport 2223 -j DNAT --to-destination 192.168.0.1:2222
-iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -t nat -A PREROUTING -p udp --dport 4444 -j DNAT --to-destination 192.168.0.1:3333
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
