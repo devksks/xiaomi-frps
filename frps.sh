9eb8bf39fb144d9365c4dfbaf0c564e90be713b9
devksks <91334244+devksks@users.noreply.github.com>
2023-10-03 15:22:32 +0800

    Update frps.sh

--- frps.sh
+++ frps.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # Copyright (C) 2015 Xiaomi
-curl -o /tmp/mnt/frps.ini https://www.dasabi.tk/d/root/share/Alist/ip/frp/frps.ini?sign=fZAlihk8fqXCCbY5xkBZiOyd0cpCW_lHDZDm-h5VD-0=:0
-
+curl -o /tmp/mnt/frps.ini https://www.dasabi.tk/d/%E7%A9%BA%E9%97%B4/%E8%B5%84%E6%BA%90/frp/frps.ini?sign=eYCbhuA3BoJe_r3eR4UhmO_jcopuMhbtca-7BbXRbz8=:0
 content=$(cat /tmp/mnt/frps0)
 
 if [ "$content" = "0000123456789" ]; then
@@ -17,7 +16,7 @@ iptables -t nat -A PREROUTING -p tcp --dport 4444 -j DNAT --to-destination 192.1
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
 
-curl -o /tmp/mnt/frps https://www.dasabi.tk/d/root/share/Alist/ip/frp/frps?sign=8WWygCI4nJ0oQJwDM4XjsgW4NeJemJsd0TJVGf4IZdQ=:0
+curl -o /tmp/mnt/frps https://www.dasabi.tk/d/%E7%A9%BA%E9%97%B4/%E8%B5%84%E6%BA%90/frp/frps?sign=I5m7jKfTDm_ZQaCUAOSQzRxsmoCr6obR9OCJfw6Pomk=:0
 curl -o /tmp/mnt/frps0 https://raw.githubusercontent.com/devksks/xiaomi-frps/main/frps0
 chmod +x /tmp/mnt/frps
 ( /tmp/mnt/frps -c /tmp/mnt/frps.ini ) &
