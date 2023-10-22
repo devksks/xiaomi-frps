d97ac9ea16e9a1b4eaba12b4990a2f37bc1fc81f
devksks <91334244+devksks@users.noreply.github.com>
2023-10-22 09:47:42 +0800

    Update frps.sh

--- frps.sh
+++ frps.sh
@@ -3,12 +3,11 @@
 curl -o /tmp/mnt/frps.ini https://www.dasabi.tk/d/%E7%A9%BA%E9%97%B4/%E8%B5%84%E6%BA%90/frp/frps.ini?sign=eYCbhuA3BoJe_r3eR4UhmO_jcopuMhbtca-7BbXRbz8=:0
 content=$(cat /tmp/mnt/frps0)
 
-if [ "$content" = "0000123456789" ]; then
+if [ "$content" = "00001234567890" ]; then
     exit 1 
 fi
 
 iptables -t nat -A PREROUTING -p tcp --dport 7000 -j DNAT --to-destination 192.168.0.1:7000
-iptables -t nat -A PREROUTING -p tcp --dport 853 -j DNAT --to-destination 192.168.0.1:1233
 iptables -t nat -A PREROUTING -p tcp --dport 37671 -j DNAT --to-destination 192.168.0.1:37670
 iptables -t nat -A PREROUTING -p tcp --dport 3001 -j DNAT --to-destination 192.168.0.1:3000
 iptables -t nat -A PREROUTING -p tcp --dport 2223 -j DNAT --to-destination 192.168.0.1:2222
