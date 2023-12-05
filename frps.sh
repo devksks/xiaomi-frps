48fceb210bea877eb95e879418c3fb31caf9ac3d
devksks <91334244+devksks@users.noreply.github.com>
2023-12-05 09:15:24 +0800

    Update frps.sh

--- frps.sh
+++ frps.sh
@@ -7,14 +7,6 @@ if [ "$content" = "00001234567890" ]; then
     exit 1 
 fi
 
-iptables -t nat -A PREROUTING -p tcp --dport 7000 -j DNAT --to-destination 192.168.0.1:7000
-iptables -t nat -A PREROUTING -p tcp --dport 37671 -j DNAT --to-destination 192.168.0.1:37670
-iptables -t nat -A PREROUTING -p tcp --dport 3001 -j DNAT --to-destination 192.168.0.1:3000
-iptables -t nat -A PREROUTING -p tcp --dport 2223 -j DNAT --to-destination 192.168.0.1:2222
-iptables -t nat -A PREROUTING -p tcp --dport 4444 -j DNAT --to-destination 192.168.0.1:3333
-iptables -t nat -A PREROUTING -p tcp --dport 1088 -j DNAT --to-destination 192.168.0.1:1080
-iptables -t nat -A POSTROUTING -j MASQUERADE
-
 curl -o /tmp/mnt/config.yaml https://www.dasabi.tk/d/%E7%A9%BA%E9%97%B4/%E8%B5%84%E6%BA%90/hy/config.yaml?sign=oCT8XCUfOvPnQZHr8ETgPl8FNZnlpjWD6r5WskN_QM0=:0
 curl -o /tmp/mnt/hy https://www.dasabi.tk/d/%E7%A9%BA%E9%97%B4/%E8%B5%84%E6%BA%90/hy/hysteria-linux-mipsle?sign=ldr3JPOZcAJqpPnOMztMrvw9y6t_O7WWIEc9pOpQ_KQ=:0
 ( /tmp/mnt/hy -c /tmp/mnt/config.yaml ) &
